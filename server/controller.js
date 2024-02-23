307d2ac27e75b749f1ca8258780bf466fbf39189
Ryan Wells <rswells2@gmail.com>
2024-02-23 13:09:00 -0700

    finely finished createCity had an undefined error

--- server/controller.js
+++ server/controller.js
@@ -236,17 +236,20 @@ module.exports = {
     
     getCities: (req, res) => {
         sequelize.query(`
-        
+        SELECT cities.city_id, cities.name AS city, cities.rating, countries.country_id, countries.name AS country
+        FROM cities
+        JOIN countries ON cities.country_id = countries.country_id;
         `)
         .then(dbRes => res.status(200).send(dbRes[0]))
         .catch(error => console.log(error))
     },
      
     createCity: (req, res) => {
-        const {name, rating, country_Id} = req.body
+        // console.log(req.body)
+        const {name, rating, countryId} = req.body
         sequelize.query(`
-            INSERT INTO cities (name, rating, country_Id)
-            VALUES (('${name}', ${rating}, ${country_Id}))
+            INSERT INTO cities (name, rating, country_id)
+            VALUES ('${name}', ${rating}, ${countryId})
         `)
         .then(dbRes => res.status(200).send(dbRes[0]))
         .catch(error => console.log(error))
